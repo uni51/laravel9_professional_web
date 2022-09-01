2d60d6443be4a7d27c6f349d612705058f624a40
Koichi.Shimomura <unieye51@gmail.com>
2022-09-01 09:43:14 +0900

    2-01-37

---
+++ database/seeders/TweetsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tweet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TweetsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Tweet::factory()->count(10)->create();
+    }
+}
