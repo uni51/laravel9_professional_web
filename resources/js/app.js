28eba4a96f358750e654b497cba00fd49fd5114b
Koichi.Shimomura <unieye51@gmail.com>
2022-09-05 11:45:35 +0900

    3-03-12

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,2 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
-// import './vue/welcome';
-
-import { createApp } from 'vue'
-
-import App from './vue/welcome.vue'
-createApp(App).mount('#welcome-vue')
