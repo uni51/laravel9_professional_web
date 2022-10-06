3b02f03c3f59c49c28b5b15d551ba369f7806018
uni51 <unieye51@gmail.com>
2022-10-06 10:00:40 +0900

    inertia利用箇所での見出し変更

--- resources/js/Pages/Tweet/TweetIndex.vue
+++ resources/js/Pages/Tweet/TweetIndex.vue
@@ -5,9 +5,9 @@ defineProps({
 </script>
 <template>
     <div class="p-4">
-        <h1 class="text-lg font-bold">ユーザ一覧</h1>
+        <h1 class="text-lg font-bold">Tweet一覧</h1>
         <ul>
-            <li v-for="tweet in tweets" :key="tweet.id">{{ tweet.content }}</li>
+            <li v-for="tweet in tweets" :key="tweet.id">・{{ tweet.content }}</li>
         </ul>
     </div>
 </template>
